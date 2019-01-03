241c7c5bf500ee54cd43cf5cc9513aec5338e048
DanielNoble <32727912+DanielNoble@users.noreply.github.com>
2019-01-03 15:57:52 -0800

    Add files via upload
    No idea if this works, but it's 8 lines so if it doesn't I'm never coding again and killing myself.

---
+++ Modulo.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Modulo {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println(scan.next() % scan.next());
+    }
+}
