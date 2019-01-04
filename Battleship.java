eb5332fdd754b3da7b82b7f64ad03fea9da30b95
DanielNoble <32727912+DanielNoble@users.noreply.github.com>
2019-01-03 21:54:34 -0800

    All conditions working: final
    Handles unbalanced serial letter and number ratio, turns 0's to 5's removes duplicates

--- Battleship.java
+++ Battleship.java
@@ -2,57 +2,40 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Battleship {
+    public static ArrayList<Integer> rows = new ArrayList<Integer>();
+    public static ArrayList<Integer> cols = new ArrayList<Integer>();
+    public static String fin = "Safe spaces are ";
     public static void main(String[] args) {
-        System.out.println("Battleship");
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Enter serial number: ");
-        String serial = scanner.next();
-
-        System.out.print("Enter num ports then num batteries + indicators: ");
-        int last = scanner.nextInt();
-
-        calc(serial, last);
+        System.out.println("Enter serial number then ports and batteries + indicators");
+        Scanner scan = new Scanner(System.in);
+        String serial = scan.next();
+        int last = scan.nextInt();
+        int currentnum;
+        String currentChar;
+        for (char current : serial.toCharArray()) {
+            try {
+                rows.add(Integer.parseInt(String.valueOf(current)));
+            } catch (NumberFormatException exc) {
+                cols.add(Character.getNumericValue(current));
+            }
+        }
+        while (!rows.isEmpty() || !cols.isEmpty()) {
+            calc(cols.remove(0), rows.remove(0));
+        }
+        calc(last / 10 - 1, last);
+        System.out.println(fin);
     }
 
-    public static void calc(String serial, int last) {
-        String rval = "";
+    public static void calc(int col, int row) {
         String alphabet = "abcdefghijklmnopqrstuvwxyz";
-        ArrayList<String> cols = new ArrayList<String>();
-        ArrayList<Integer> rows = new ArrayList<Integer>();
-        // Calculate safe spaces with serial number
-        for (int i = 0; i < serial.length(); i++) {
-            String currentChar = serial.substring(i, i+1);
-            if (currentChar.equals("0")) {
-                currentChar = "5";
-            }
-            try {
-                rows.add(Integer.parseInt(currentChar));
-            } catch (NumberFormatException exc) {
-                cols.add(currentChar);
-            }
-        }
-        // Modulo letters in the columns arrayList
-        for (int i = 0; i < cols.size(); i++) {
-            int modCharPos = alphabet.indexOf(cols.get(i)) % 5;
-            cols.set(i, alphabet.substring(modCharPos, modCharPos + 1));
-        }
-        // Modulo numbers in the rows arrayList
-        for (int i = 0; i < rows.size(); i++) {
-            rows.set(i, rows.get(i) % 5);
-        }
-        // Add modulo'd coordinates to rval
-        while (cols.size() > 0 && rows.size() > 0) {
-            if (rval.indexOf(cols.get(0) + "" + rows.get(0)) == -1) { // ensures unique new safe space
-                rval += cols.remove(0) + rows.remove(0) + " ";
-            }
-        }
-
-
-        // Calculate last safe space
-        int mod5 = (last / 10 - 1) % 5;
-        rval += "and " + alphabet.substring(mod5, mod5 + 1) + "" + (last % 5);
-
-        System.out.println("Safe spaces are " + rval);
+        col %= 5;
+        row %= 5;
+        if (row == 0)
+            row += 5;
+        if (col == 0)
+            col += 5;
+        String column = alphabet.substring(col, col + 1);
+        if (fin.indexOf(column + row) == -1)
+            fin += column + row + " ";
     }
 }
