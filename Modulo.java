ec1a810dfb6554706f8229ab8a31af67416e7ba3
DanielNoble <32727912+DanielNoble@users.noreply.github.com>
2019-01-03 16:10:04 -0800

    Update Modulo.java

--- Modulo.java
+++ Modulo.java
@@ -3,6 +3,6 @@ import java.util.Scanner;
 public class Modulo {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println(scan.next() % scan.next());
+        System.out.println(scan.nextInt() % scan.nextInt());
     }
 }
